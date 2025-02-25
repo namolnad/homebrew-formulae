e976d1911a2119279cd1cfe06cd77c2ebe39a834
Dan Loman <daniel.h.loman@gmail.com>
2025-02-24 18:18:04 -0800

    Update ref and expected test value

--- Formula/display-arranger.rb
+++ Formula/display-arranger.rb
@@ -2,8 +2,8 @@ class DisplayArranger < Formula
   desc "Command-line application for your Mac designed to make interacting with and arranging your external displays a bit simpler"
   homepage "https://github.com/namolnad/display-arranger"
   url "https://github.com/namolnad/display-arranger.git",
-      tag:      "0.2.0",
-      revision: "7882845f399242bfddfdf1f0d01491133f8b0d6c"
+      tag:      "0.3.1",
+      revision: "2aff6965a3138a89e5bb6cfd9a6fabb8d0ce859b"
   license "MIT"
   head "https://github.com/namolnad/display-arranger.git", branch: "main"
 
@@ -19,30 +19,36 @@ class DisplayArranger < Formula
   test do
     expected_output = <<~EOS
             Use display-arranger to get information about your current displays,
-            for setting the main display, and for setting the positions of your other
-            connected displays relative to another display's position (defaults to main)
+            for setting the primary display, and for setting the positions of your other
+            connected displays relative to another display's position (defaults to primary)
 
             Usage: display-arranger
-            [-h, --help] Shows the help text
-            [--ids] Returns the ids for all connected displays
-            [-i, --info] Shows information about the connected displays
-            [--moveMouse] Moves the mouse cursor to the given coordinates on the main display (example: 100-100)
-            [--setMain <DisplayId>] Pass the id of the display that you want to make the main display (dock/menu bar)
-            [--supportedPositions] Displays a list of the supported positions for --otherPosition command
-            [-op, --otherPosition <DisplayId> <Position> <ReferenceDisplayId>] Used in conjunction with --setMain to
-            control the position of your other displays. Order is important, see README for additional usage details.
 
+            # Basic/Info commands
+            [-h, --help] Shows the help text
+            [-v, --version] Shows the version of the application
+
+            # Query/List commands
+            [-i, --info] Shows information about the connected displays
+            [-l, --list-displays] Returns the ids for all connected displays
+            [-L, --list-positions] Displays a list of the supported positions for --arrange argument
+
+            # Action commands
+            [-p, --primary <DisplayId>] Pass the id of the display that you want to make the primary display (dock/menu bar)
+            [-a, --arrange <DisplayId> <Position> <ReferenceDisplayId>] Used in conjunction with --primary to
+            control the position of your other displays. Order is important, see README for additional usage details.
+            [-m, --mouse] Moves the mouse cursor to the given coordinates on the primary display (example: 100,100)
 
             Examples:
             display-arranger --info
             Returns information about your attached displays
 
-            display-arranger --setMain 69670848 -op 54019204 onLeft-bottomAligned
-            Makes the display with the DisplayId 69670848 the main display and
-            positions 54019204 to the left of, bottom-aligned to, the main display
+            display-arranger --primary 69670848 -a 54019204 on-left:bottom-aligned
+            Makes the display with the DisplayId 69670848 the primary display and
+            positions 54019204 to the left of, and bottom-aligned to, the primary display
 
             Note: Global Position's origin is the upper-left corner of the display
     EOS
-    assert_match expected_output, shell_output("#{bin}/display-arranger --help").chomp
+    assert_equal expected_output.strip, shell_output("#{bin}/display-arranger --help").strip
   end
 end
